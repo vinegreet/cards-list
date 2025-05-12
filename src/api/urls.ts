ea247ca429b20e1e559aa61b62305df4be946c85
Roman Zots <vinegreet@gmail.com>
2025-05-12 23:41:00 +0300

    Fix comments

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -5,10 +5,9 @@
 const httpSchema = 'https://';
 const apiUrl = 'staging-api.coing.co';
 const endpoint = 'api/v2/communities/838/groups';
-// Removed static page and pageSize, kept other fixed params
 const fixedParams = '&filterBy[closed]=0&filterBy[isPrivate]=0'; 
 
-// dataUrl is now a function to allow dynamic page and pageSize
+// dataUrl is a function to allow dynamic page number and pageSize (batch size)
 export const getDataUrl = (page: number, pageSize: number): string => {
   return `${httpSchema}${apiUrl}/${endpoint}?page=${page}&pageSize=${pageSize}${fixedParams}`;
 };
